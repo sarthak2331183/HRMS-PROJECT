bfba64b9fae0e6287a6837c3edf46e6644d05f06
sarthak2331183 <np03cs4a220314@heraldcollege.edu.np>
2024-04-03 16:05:10 +0545

    Updated admin dashboard component

--- myapp/src/component/admin/Dashboard.jsx
+++ myapp/src/component/admin/Dashboard.jsx
@@ -41,6 +41,7 @@ const navigate = useNavigate();
 
   return (
     <div className="container">
+      
       {/* aside section starts*/}
       <aside>
         <div className="top">
@@ -86,7 +87,7 @@ const navigate = useNavigate();
 
       {/* main section starts*/}
       <main>
-        <h1>Dashboard</h1>
+        <h1>Hello</h1>
         <div className="date">
           <input type="date" />
         </div>
@@ -94,60 +95,24 @@ const navigate = useNavigate();
         <div className="inside">
           {/* start selling */}
           <div className="sales">
-            <span className="material-symbols-outlined">trending_up</span>
-            <div className="middle">
-              <div className="left">
-                <h3>Total Sales</h3>
-                <h1>$35,000</h1>
-              </div>
-              <div className="progress">
-                <svg>
-                  <circle r="30" cy="40" cx="40"></circle>
-                </svg>
-                <div className="number">80%</div>
-              </div>
-            </div>
-            <small>Last 24 Hours</small>
+              <h3>On duty</h3>
+              <p>3</p>
+
+
           </div>
           {/* ends selling */}
 
           {/* start expenses */}
           <div className="expenses">
-            <span className="material-symbols-outlined">local_mall</span>
-            <div className="middle">
-              <div className="left">
-                <h3>Total Expenses</h3>
-                <h1>$35,000</h1>
-              </div>
-              <div className="progress">
-                <svg>
-                  <circle r="30" cy="40" cx="40"></circle>
-                </svg>
-                <div className="number">80%</div>
-              </div>
-            </div>
-            <small>Last 24 Hours</small>
+          <h3>On duty</h3>
+              <p>3</p>
           </div>
           {/* ends expenses */}
 
           {/* start income*/}
           <div className="income">
-            <span className="material-symbols-outlined">
-              stacked_line_chart
-            </span>
-            <div className="middle">
-              <div className="left">
-                <h3>Total income</h3>
-                <h1>$35,000</h1>
-              </div>
-              <div className="progress">
-                <svg>
-                  <circle r="30" cy="40" cx="40"></circle>
-                </svg>
-                <div className="number">80%</div>
-              </div>
-            </div>
-            <small>Last 24 Hours</small>
+          <h3>On duty</h3>
+              <p>3</p>
           </div>
           {/* ends income */}
         </div>
@@ -155,14 +120,14 @@ const navigate = useNavigate();
 
         {/* start recent order */}
         <div className="recent_order">
-          <h1>Recent Orders</h1>
+          <h1>New Joins</h1>
           <table>
             <thead>
               <tr>
-                <th>Product Name</th>
-                <th>Product ID</th>
-                <th>Payments</th>
-                <th>Status</th>
+                <th>Full Name</th>
+                <th>Location</th>
+                <th>Branch</th>
+                {/* <th>Status</th> */}
               </tr>
             </thead>
             <tbody>
@@ -170,31 +135,31 @@ const navigate = useNavigate();
                 <td>Innovate Suite</td>
                 <td>IS-2023</td>
                 <td className="danger">Pending</td>
-                <td className="warning">Processing</td>
+                {/* <td className="warning">Processing</td> */}
               </tr>
               <tr>
                 <td>CloudConnect</td>
                 <td>CC-2452</td>
                 <td className="danger">Paid</td>
-                <td className="warning">Delivered</td>
+                {/* <td className="warning">Delivered</td> */}
               </tr>
               <tr>
                 <td>NetGuard Pro</td>
                 <td>NG-2314</td>
                 <td className="danger">Pending</td>
-                <td className="warning">Processing</td>
+                {/* <td className="warning">Processing</td> */}
               </tr>
               <tr>
                 <td>SmartTrack Lite</td>
                 <td>ST-6563</td>
                 <td className="danger">Pending</td>
-                <td className="warning">Processing</td>
+                {/* <td className="warning">Processing</td> */}
               </tr>
               <tr>
                 <td>DataScape Pro</td>
                 <td>DS-5643</td>
                 <td className="danger">Paid</td>
-                <td className="warning">Shipped</td>
+                {/* <td className="warning">Shipped</td> */}
               </tr>
             </tbody>
           </table>
@@ -232,6 +197,9 @@ const navigate = useNavigate();
       <div className="circle">
         <span className="number">1</span>
       </div>
+
+
+
       <div className="details">
         <p>
           <b>Team Meeting</b>
@@ -244,6 +212,9 @@ const navigate = useNavigate();
         <span className="material-symbols-outlined">more_vert</span>
       </div>
     </div>
+
+
+
 
     <div className="meeting">
       <div className="circle">
